224deb3a539f6a364fc2fb5512f0a418f5374787
Dionuta <diounta@gmail.com>
2019-02-09 10:16:45 -0800

    MVP DONE

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -30,37 +30,39 @@ class TabLink {
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+    const tabs = document.querySelectorAll(".tab");
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+     tabs.forEach(tab => tab.classList.remove("active-tab"));
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+    const cards = document.querySelectorAll(".card");
 
     // Iterate through the NodeList setting the display style each one to 'none'
-    // cards.forEach()
+    cards.forEach(card => card.style.display = "none");
     
     // Add a class of ".active-tab" to this.tabElement
     // this.tabElement;
-  
+    this.tabElement.classList.add("active-tab");
+   
     // Notice we are looping through the this.cards array and invoking selectCard() from the TabCard class. Just un-comment the code and study what is happening here.
-    // this.cards.forEach(card => card.selectCard());
+    this.cards.forEach(card => card.selectCard());
   }
 }
+
 
 class TabCard {
   constructor(cardElement){
     // Assign this.cardElement to the cardElement DOM reference
-    // this.cardElement;
+    this.cardElement = cardElement;
   }
   selectCard(){
     // Update the style of this.cardElement to display = "flex"
-    // this.cardElement;
+    this.cardElement.style.display = "flex"
   }
 
 }
-let tabs = document.querySelectorAll();
+
 /* START HERE: 
 
 - Select all classes named ".tab" and assign that value to the tabs variable
@@ -75,4 +77,4 @@ let tabs = document.querySelectorAll('.tab');
 //Test = Passed log
 // console.log(tabs);
 
-tabs.forEach(tab => new Tablink (tab) )
+tabs.forEach(tab => new TabLink(tab));
